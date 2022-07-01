2c298bc6f4bc7e7179adabf7e1f387f9db3dfad6
Marie <mariezarrini@gmail.com>
2022-07-01 15:43:34 +0430

    Create a new class for reusable try-catch.

---
+++ ReusableTryCatch/SafeExecutor.cs
@@ -0,0 +1,29 @@
+﻿namespace ReusableTryCatch
+{
+	internal class SafeExecutor
+	{
+		public static void TryExecutingAction(Action action)
+		{
+			try
+			{
+				action();
+			}
+			catch (NullReferenceException)
+			{
+				Console.WriteLine("Null Reference Exception.");
+			}
+			catch (ArgumentException)
+			{
+				Console.WriteLine("Argument Exception.");
+			}
+			catch (Exception)
+			{
+				Console.WriteLine("Something bad happens.");
+			}
+			finally
+			{
+				Console.WriteLine("All Done.");
+			}
+		}
+	}
+}
